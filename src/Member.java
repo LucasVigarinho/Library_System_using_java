cabee294b0c0cd7fcb943a97ec2b6c93af6bb10e
LucasVigarinho <lucas.maximo.mv@gmail.com>
2019-11-20 13:32:26 +0000

    commit @LucasVigarinho adjustments in the staff class and members class

--- src/Member.java
+++ src/Member.java
@@ -6,21 +6,15 @@ public class Member {
 	private String memberName;
 	private String memberAddress;
 	private String since;
-	private boolean staffA;
-	private String position;
-	private String until;
-	private String password;
+
 	
 	//create the constructor
-	public Member(String memberId, String memberName, String memberAddress, String since, boolean staffA,  String position, String until, String password) {
+	public Member(String memberId, String memberName, String memberAddress, String since) {
 		this.memberId = memberId;
 		this.memberName = memberName;
 		this.memberAddress = memberAddress;
 		this.since = since;
-		this.staffA = staffA;
-		this.position = position;
-		this.until = until;
-		this.password = password;
+
 	}//close the constructor
 
 	/** Create a getter for getting the member Id  (as e-mail) */
@@ -35,7 +29,6 @@ public class Member {
 	public String getMemberName() {
 		return memberName;
 	}//closing getMemberName
-
 	/** Create a getter for getting member address*/
 	public String getMemberAddress() {
 		return memberAddress;
@@ -48,38 +41,7 @@ public class Member {
 	public String getSince() {
 		return since;
 	}//closing getSince
-	/** Create a getter for getting member situation*/
-	public boolean getStaffA() {
-		return this.staffA;
-	}//closing getMemberAddress
-	/** Create a setter for setting a member address, giving the opportunity to change or update address*/
-	public void setStaffA(boolean staffA) {
-		this.staffA = staffA;
-	}//closing getMemberAddress
-	/** Created a getter for getting the position of the Staff */
-	public String getPosition() {
-		return position;
-	}//closing getPosition 
-	/** Created a setter for setting or update the position of the Staff */
-	public void setPosition(String position) {
-		this.position = position;
-	}//closing setPOsition 
-	/** Created a getter for getting the until (last day) of the Staff */
-	public String getUntil() {
-		return until;
-	}//closing getUntil
-	/** Created a setter for setting the until (last day) of the Staff */
-	public void setUntil(String until) {
-		this.until = until;
-	}//closing setUntil
-	/** Created a getter for getting the password of the Staff */
-	public String getPassword() {
-		return password;
-	}//closing getPassword
-	/** Created a setter for setting the password of the Staff */
-	public void setPassword(String password) {
-		this.password = password;
-	}//closing setPassword
+	
 	
 	
 	
--- src/Staff.java
+++ src/Staff.java
@@ -1,46 +1,56 @@
-//
-///** Creating a user class **/
-//public class Staff extends Member{
-//
-//	//create the default attributes
-//	private String position;
-//	private String until;
-//	private String password;
-//	
-//	//create the constructor
-//	public Staff(String memberId, String memberName, String memberAddress, 
-//			  String since, boolean staffA, String position, String until, String password ) {
-//		super(memberId, memberName,memberAddress,since, staffA);
-//		this.position = position;
-//		this.until = until;
-//		this.password = password;
-//	}//close the constructor
-//
-//	/** Created a getter for getting the position of the Staff */
-//	public String getPosition() {
-//		return position;
-//	}//closing getPosition 
-//	/** Created a setter for setting or update the position of the Staff */
-//	public void setPosition(String position) {
-//		this.position = position;
-//	}//closing setPOsition 
-//	/** Created a getter for getting the until (last day) of the Staff */
-//	public String getUntil() {
-//		return until;
-//	}//closing getUntil
-//	/** Created a setter for setting the until (last day) of the Staff */
-//	public void setUntil(String until) {
-//		this.until = until;
-//	}//closing setUntil
-//	/** Created a getter for getting the password of the Staff */
-//	public String getPassword() {
-//		return password;
-//	}//closing getPassword
-//	/** Created a setter for setting the password of the Staff */
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}//closing setPassword
-//	
-//	
-//	
-//}//closing the class member
+
+/** Creating a user class **/
+public class Staff extends Member{
+
+	//create the default attributes
+	private String position;
+	private String until;
+	private String password;
+	private boolean staffA;
+	
+	//create the constructor
+	public Staff(String memberId, String memberName, String memberAddress, 
+			  String since, boolean staffA, String position, String until, String password ) {
+		super(memberId, memberName,memberAddress,since);
+		this.position = position;
+		this.until = until;
+		this.password = password;
+		this.staffA = staffA;
+	}//close the constructor
+
+
+	/** Created a getter for getting the position of the Staff */
+	public String getPosition() {
+		return position;
+	}//closing getPosition 
+	/** Created a setter for setting or update the position of the Staff */
+	public void setPosition(String position) {
+		this.position = position;
+	}//closing setPOsition 
+	/** Created a getter for getting the until (last day) of the Staff */
+	public String getUntil() {
+		return until;
+	}//closing getUntil
+	/** Created a setter for setting the until (last day) of the Staff */
+	public void setUntil(String until) {
+		this.until = until;
+	}//closing setUntil
+	/** Created a getter for getting the password of the Staff */
+	public String getPassword() {
+		return password;
+	}//closing getPassword
+	/** Created a setter for setting the password of the Staff */
+	public void setPassword(String password) {
+		this.password = password;
+	}//closing setPassword
+	/** Created a getter for getting the boolean of the StaffA situation */
+	public boolean getStaffA() {
+		return staffA;
+	}//closing getStaffA
+	/** Created a getter for getting the situation of the Staff */
+	public void setStaffA(boolean staffA) {
+		this.staffA = staffA;
+	}//closing setStaffA
+	
+	
+}//closing the class member
